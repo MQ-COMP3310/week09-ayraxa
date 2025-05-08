5fd269eb434e4ab43346b757e76481ed1c9badc2
ayraxa <96117601+ayraxa@users.noreply.github.com>
2025-05-08 14:29:01 +1000

    debug mode off

--- run.py
+++ run.py
@@ -2,5 +2,8 @@ from project import create_app
 
 if __name__ == '__main__':
   app = create_app()
-  app.run(host = '0.0.0.0', port = 8001, debug=True) 
+  ##app.run(host = '0.0.0.0', port = 8001, debug=True) 
+  ##debug mode off
+  app.run(host='0.0.0.0', port=8001, debug=False)
+
 
